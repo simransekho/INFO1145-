b10b0067200581b411908ab8bb9dbb39c2cb9ef0
Balraj singh <148562133+simransekho@users.noreply.github.com>
2023-10-31 16:37:15 -0400

    Create code.py
    This is my first code file.

---
+++ code.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv('C:\\Users\\siman OneDrive\\Desktop\\PROG8469\\Python\\train.csv")
+= shape and data types of the data
+print (df.shape)
+print(df.dtypes)
+print (df.dtypes to string())
